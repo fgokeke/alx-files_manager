dfd114e57aa973282afbc0307150746035c25f40
okekefeargod <okekefeargod@gmail.com>
2024-06-19 09:54:28 -0700

    2. First API

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { router as indexRouter } from './routes/index.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;
